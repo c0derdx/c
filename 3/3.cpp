a3c9cc06108b00857a8c3423c7b33f6293c144e8
c0derdx <75431371+c0derdx@users.noreply.github.com>
2021-09-08 19:40:12 +0530

    update

--- 3/3.cpp
+++ 3/3.cpp
@@ -106,6 +106,69 @@ void parallelogram() {
     
 }
 
+void mirroredParallelogram() {
+
+    for (int i = 1; i <=5; ++i)
+    {
+        for (int j = 5; j >i; --j)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j<= 5; j++)
+        {
+            cout << "*";
+        }
+
+        cout <<endl;
+        
+        
+    }
+
+}
+
+void pyramid(){
+    for (int i = 1; i <=5; ++i)
+    {
+        for (int j = 5; j >i; --j)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j<= 2*i - 1; j++)
+        {
+            cout << "*";
+        }
+
+        cout <<endl;      
+        
+    }
+    
+}
+
+void invertedPyramid(){
+    for (int i = 0; i <5; ++i)
+    {
+        for (int j = 1; j <=i; ++j)
+        {
+            cout << " ";
+        }
+
+        for (int j = 5; j>= 2*i-3; j--)
+        {
+            cout << "*";
+        }
+
+        cout <<endl;      
+        
+    }
+    
+}
+
+void pascalTriangle(){
+        
+}
+
 int main()
 {
     // rightTriangle();
@@ -114,4 +177,8 @@ int main()
     // hollowRectangle();
     // hollowDowntraingle();
     // parallelogram();
+    // mirroredParallelogram();
+    // pyramid();
+    // invertedPyramid();
+    // pascalTriangle();
 }
