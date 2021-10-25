75f6a005812a966c79dccaa30834a52a24c6d052
c0derdx <75431371+c0derdx@users.noreply.github.com>
2021-10-25 16:32:13 +0530

    commit

--- 3/3.cpp
+++ 3/3.cpp
@@ -169,6 +169,66 @@ void pascalTriangle(){
         
 }
 
+void butterflyPattern() {
+    int n;
+    cin>> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <=i; j++)
+        {
+            cout<<"* ";
+        }
+        for (int j = 1; j<= 2*n-2*i; j++)
+        {
+            cout<<"  ";
+        }
+        for (int j = 1; j <=i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <=i; j++)
+        {
+            cout<<"* ";
+        }
+        for (int j = 1; j<= 2*n-2*i; j++)
+        {
+            cout<<"  ";
+        }
+        for (int j = 1; j <=i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+}
+
+void questionPattern() {
+    int n;
+    cin>> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <=i; j++)
+        {
+            cout<<" # ";
+        }
+        for (int j = 1; j<= 2*n-2*i; j++)
+        {
+            cout<<" * ";
+        }
+        for (int j = 1; j <=i; j++)
+        {
+            cout<<" # ";
+        }
+        cout<<endl;
+    }    
+}
+
 int main()
 {
     // rightTriangle();
@@ -181,4 +241,6 @@ int main()
     // pyramid();
     // invertedPyramid();
     // pascalTriangle();
+    // butterflyPattern();
+    questionPattern();
 }
